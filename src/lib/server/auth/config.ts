8af28f3440f7b3b2cebebd40eaa9f7b5b0046998
Tomasz Kalisiak <tomasz.kalisiak3@gmail.com>
2026-03-26 01:49:47 +0100

    Configure resend

--- src/lib/server/auth/config.ts
+++ src/lib/server/auth/config.ts
@@ -7,6 +7,7 @@ import { sveltekitCookies } from 'better-auth/svelte-kit';
 import { getRequestEvent } from '$app/server';
 import { env } from '$env/dynamic/private';
 import { captcha, openAPI } from 'better-auth/plugins';
+import { resend } from '$lib/server/email/resend';
 
 export const auth = betterAuth({
 	database: drizzleAdapter(db, {
@@ -20,12 +21,23 @@ export const auth = betterAuth({
 	}),
 	emailAndPassword: {
 		enabled: true,
-		sendResetPassword: async (/*{ user, url, token }, request*/) => {
-			// void sendEmail({
-			// 	to: user.email,
-			// 	subject: 'Reset your password',
-			// 	text: `Click the link to reset your password: ${url}`
-			// });
+		sendResetPassword: async ({ user, url }) => {
+			await resend.emails.send({
+				from: 'RhythmGame <noreply@rhythmgame.eu>',
+				to: user.email,
+				subject: 'Reset your password',
+				html: `<p>Click <a href="${url}">here</a> to reset your password.</p>`
+			});
+		}
+	},
+	emailVerification: {
+		sendVerificationEmail: async ({ user, url }) => {
+			await resend.emails.send({
+				from: 'RhythmGame <noreply@rhythmgame.eu>',
+				to: user.email,
+				subject: 'Verify your email',
+				html: `<p>Click <a href="${url}">here</a> to verify your email.</p>`
+			});
 		}
 	},
 	// requireEmailVerification: true,
@@ -41,7 +53,7 @@ export const auth = betterAuth({
 		captcha({
 			provider: 'cloudflare-turnstile',
 			secretKey: env.TURNSTILE_SECRET_KEY!,
-			endpoints: ["/sign-up/email", "/forget-password"]
+			endpoints: ['/sign-up/email', '/forget-password']
 		}),
 		sveltekitCookies(getRequestEvent)
 	]
