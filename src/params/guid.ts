c14151dd22a2414b49abadedf803e02786059285
Tomasz Kalisiak <tomasz.kalisiak3@gmail.com>
2026-04-04 10:18:00 +0200

    Make braces optional in matcher

--- src/params/guid.ts
+++ src/params/guid.ts
@@ -1,7 +1,7 @@
 import type { ParamMatcher } from '@sveltejs/kit';
 
 export const match: ParamMatcher = (param) => {
-	return /^\{[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}}$/.test(param);
+	return /^{?[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}}?$/i.test(param);
 };
 
 
